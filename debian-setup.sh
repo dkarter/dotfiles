8cb556bb880d9a125fc05ccc15df6347ee23b036
Dorian Karter <dkarter@users.noreply.github.com>
2021-08-26 21:05:20 -0500

    Add script for setting up Debian

---
+++ debian-setup.sh
@@ -0,0 +1,26 @@
+# install all debian packages needed for my dotfiles
+sudo apt -y install \
+  zsh \
+  bat \
+  libssl-dev \
+  automake \
+  autoconf \
+  libncurses5-dev \
+  ruby \
+  bison \
+  fop \
+  xsltproc \
+  libgl1-mesa-dev \
+  libglu1-mesa-dev \
+  libpng-dev \
+  wx-common \
+  libssl-dev \
+  libssh-dev \
+  unixodbc-dev \
+  libxml2-utils \
+  libncurses-dev \
+  openjdk-11-jdk \
+  libwxgtk3.0-gtk3-dev \
+  libgl1-mesa-dev \
+  libglu1-mesa-dev \
+  m4
