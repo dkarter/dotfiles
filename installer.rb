1da1e0bd64a40dcb94ceb0752d49f282d7eb5f8a
Dorian Karter <dkarter@users.noreply.github.com>
2022-01-24 17:42:40 -0600

    Extract string monkeypatch to new file

--- installer.rb
+++ installer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'fileutils'
+require_relative 'installer/string'
 
 ASDF_INSTALL_DIR = '~/.asdf'
 GIT_TEMPLATE_INSTALL_DIR = '~/.git_template'
@@ -293,34 +294,5 @@ class Installer
   end
 end
 
-class String
-  def colorize(color_code)
-    "\e[#{color_code}m#{self}\e[0m"
-  end
-
-  def red
-    colorize(31)
-  end
-
-  def green
-    colorize(32)
-  end
-
-  def yellow
-    colorize(33)
-  end
-
-  def blue
-    colorize(34)
-  end
-
-  def pink
-    colorize(35)
-  end
-
-  def light_blue
-    colorize(36)
-  end
-end
 
 Installer.new.install
