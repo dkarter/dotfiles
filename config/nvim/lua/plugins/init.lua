091d6dcb4ceeb56648bb52caa474332f4505624d
Dorian Karter <dkarter@users.noreply.github.com>
2022-04-24 16:30:42 -0500

    Vim: Use tokyonight theme

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -13,51 +13,11 @@ return packer.startup(function(use)
 
   -- color schemes
   ----------------
-
   use({
-    "marko-cerovac/material.nvim",
+    "folke/tokyonight.nvim",
     config = function()
-      vim.g.material_style = "palenight"
-
-      require("material").setup({
-        contrast = {
-          sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-          floating_windows = false, -- Enable contrast for floating windows
-          line_numbers = false, -- Enable contrast background for line numbers
-          sign_column = false, -- Enable contrast background for the sign column
-          cursor_line = false, -- Enable darker background for the cursor line
-          non_current_windows = false, -- Enable darker background for non-current windows
-          popup_menu = false, -- Enable lighter background for the popup menu
-        },
-
-        italics = {
-          comments = true, -- Enable italic comments
-          keywords = false, -- Enable italic keywords
-          functions = false, -- Enable italic functions
-          strings = false, -- Enable italic strings
-          variables = false, -- Enable italic variables
-        },
-
-        contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
-          "terminal", -- Darker terminal background
-          "packer", -- Darker packer background
-          "qf", -- Darker qf list background
-        },
-
-        disable = {
-          borders = false, -- Disable borders between verticaly split windows
-          background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
-          term_colors = false, -- Prevent the theme from setting terminal colors
-          eob_lines = false, -- Hide the end-of-buffer lines
-        },
-
-        high_visibility = {
-          lighter = false, -- Enable higher contrast text for lighter style
-          darker = false, -- Enable higher contrast text for darker style
-        },
-      })
       -- set colorscheme
-      vim.cmd([[colorscheme material]])
+      vim.cmd([[colorscheme tokyonight]])
     end,
   })
 
