2744c09ef4ca2de77ae9a42a7443f31407a1a310
Dorian Karter <dkarter@users.noreply.github.com>
2022-04-25 18:12:01 -0500

    feat(nvim): add completion kind icons :sparkles:
    Makes it easier to tell what the thing is (Function/Variable etc..)

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -11,6 +11,34 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
 end
 
+local kind_icons = {
+  Text = '',
+  Method = '',
+  Function = '',
+  Constructor = '',
+  Field = '',
+  Variable = '',
+  Class = 'ﴯ',
+  Interface = '',
+  Module = '',
+  Property = 'ﰠ',
+  Unit = '',
+  Value = '',
+  Enum = '',
+  Keyword = '',
+  Snippet = '',
+  Color = '',
+  File = '',
+  Reference = '',
+  Folder = '',
+  EnumMember = '',
+  Constant = '',
+  Struct = '',
+  Event = '',
+  Operator = '',
+  TypeParameter = '',
+}
+
 local M = {}
 
 M.setup = function()
@@ -20,10 +48,22 @@ M.setup = function()
         luasnip.lsp_expand(args.body)
       end,
     },
-    -- window = {
-    --   completion = cmp.config.window.bordered(),
-    --   documentation = cmp.config.window.bordered(),
-    -- },
+
+    formatting = {
+      format = function(entry, vim_item)
+        -- find icon based on kind
+        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+        -- Source
+        vim_item.menu = ({
+          buffer = '[Buffer]',
+          nvim_lsp = '[LSP]',
+          luasnip = '[LuaSnip]',
+          nvim_lua = '[NvimLua]',
+        })[entry.source.name]
+        return vim_item
+      end,
+    },
+
     mapping = cmp.mapping.preset.insert {
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
