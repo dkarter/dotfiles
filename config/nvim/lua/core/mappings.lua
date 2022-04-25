f42f40ef7b56f466952e9eac7edc025511f3c750
Dorian Karter <dkarter@users.noreply.github.com>
2022-04-25 18:09:38 -0500

    refactor(nvim): move lsp mappings to mappings file
    This will allow managing all mappings centrally, while also making it
    easy to remove when plugins eventually change.

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -22,10 +22,47 @@ nmap { 'N', 'Nzzzv' }
 nmap { '<c-w>w', ':bd<CR>' }
 
 -- rename current file
-nmap { '<Leader>rn', ":Move <C-R>=expand('%')<CR>" }
+nmap { '<Leader>RN', ":Move <C-R>=expand('%')<CR>" }
 
 -- sort selected lines
 vmap { 'gs', ':sort<CR>' }
 
 -- remove highlighting on escape
 nmap { '<esc>', ':nohlsearch<cr>', { silent = true } }
+
+local M = {}
+
+M.lsp_mappings = function(bufnr)
+  local opts = { noremap = true, silent = true }
+
+  local buf_nmap = function(mapping, cmd)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', mapping, cmd, opts)
+  end
+
+  -- TODO: expose this as a function from mappings module e.g. require('core.mappings').lsp_mappings()
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_nmap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+  buf_nmap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+  buf_nmap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+  buf_nmap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+  buf_nmap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  buf_nmap('<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+  buf_nmap('<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+  buf_nmap('<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+  buf_nmap('<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+  buf_nmap('<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+  buf_nmap('<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+  buf_nmap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+  buf_nmap('<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+end
+
+M.lsp_diagnostic_mappings = function()
+  local opts = { noremap = true, silent = true }
+  nmap { '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts }
+  nmap { '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts }
+  nmap { ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts }
+  nmap { '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts }
+end
+
+return M
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -9,11 +9,7 @@ local M = {}
 M.setup = function()
   -- TODO: expose this as a function from mappings module e.g. require('core.mappings').diagnostic_mappigns()
   -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-  local opts = { noremap = true, silent = true }
-  vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  require('core.mappings').lsp_diagnostic_mappings()
 
   -- Use an on_attach function to only map the following keys
   -- after the language server attaches to the current buffer
@@ -21,29 +17,7 @@ M.setup = function()
     -- Enable completion triggered by <c-x><c-o> (not sure this is necessary with
     -- vmp plugin)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- TODO: expose this as a function from mappings module e.g. require('core.mappings').lsp_mappings()
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(
-      bufnr,
-      'n',
-      '<leader>wl',
-      '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-      opts
-    )
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    require('core.mappings').lsp_mappings(bufnr)
   end
 
   -- Use a loop to conveniently call 'setup' on multiple servers and
