988cf7da6ae21af6085fb06f81d3d6c6b7c7f4f8
Dorian Karter <dkarter@users.noreply.github.com>
2022-04-27 12:08:14 -0500

    chore(nvim): remove unnecessary runtimepath

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,8 +24,6 @@ end
 -- iterative migration: source old config first, then overwrite it with new
 -- configs
 -- source a vimscript file
-vim.cmd 'set runtimepath^=~/.vim runtimepath+=~/.vim/after'
-vim.cmd 'let &packpath = &runtimepath'
 vim.cmd 'source ~/.vimrc'
 
 -- require `new_config.lua` from the nvim/lua folder:
