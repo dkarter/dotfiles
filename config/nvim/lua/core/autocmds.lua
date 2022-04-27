4df91f51ba9ee6d97388639c47c95f5361b37111
Dorian Karter <dkarter@users.noreply.github.com>
2022-04-27 17:48:05 -0500

    feat(nvim): check spelling in text/markdown files

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -1,17 +1,7 @@
 -- put event listeners here
--- local generic_group = vim.api.nvim_create_augroup("GenericGroup", {})
 
--- -- Lua function
--- local myluafun = function()
---   print("entered buffer")
---   print(vim.bo.filetype)
--- end
-
--- -- When editing a file, always jump to the last known cursor position.
--- -- Don't do it for commit messages, when the position is invalid, or when
--- -- inside an event handler (happens when dropping a file on gvim).
--- vim.api.nvim_create_autocmd({ "BufReadPost" }, {
---   pattern = { "*" },
---   callback = myluafun,
---   group = generic_group,
--- })
+-- spell check markdown and txt
+vim.api.nvim_create_autocmd(
+  { 'BufRead', 'BufNewFile' },
+  { pattern = { '*.txt', '*.md', '*.tex' }, command = 'setlocal spell' }
+)
