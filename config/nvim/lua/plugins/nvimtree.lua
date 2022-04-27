6d1b03a07f686432696db73eaeef5d738abd9b1e
Dorian Karter <dkarter@users.noreply.github.com>
2022-04-27 11:57:20 -0500

    fix(nvim): change config for NvimTree
    There were some breaking changes and I had settings that were
    incompatible as well as unneeded settings (because they were already the
    default)

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -1,9 +1,6 @@
 local g = vim.g
 local keymap = vim.api.nvim_set_keymap
 
--- is this line necessary???
--- local action = require("nvim-tree.config").nvim_tree_callback
-
 g.nvim_tree_show_icons = {
   git = 1,
   folders = 1,
@@ -31,22 +28,11 @@ if not present then
   return
 end
 
-local default = {
-  disable_netrw = false,
+local config = {
   hijack_netrw = false,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_close = false,
-  auto_reload_on_write = true,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = false,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
+    show_on_dirs = false,
     icons = {
       hint = '',
       info = '',
@@ -54,82 +40,17 @@ local default = {
       error = '',
     },
   },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
   filters = {
     dotfiles = false,
     custom = { '.DS_Store', 'fugitive:', '.git' },
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {},
-    },
-    number = false,
-    relativenumber = false,
-    signcolumn = 'yes',
-  },
-  trash = {
-    cmd = 'trash',
-    require_confirm = true,
-  },
-  actions = {
-    change_dir = {
-      global = false,
-    },
-    open_file = {
-      quit_on_open = false,
-    },
-  },
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-  },
-  icons = {
-    default = '',
-    symlink = '',
-    git = {
-      unstaged = '',
-      staged = 'S',
-      unmerged = '',
-      renamed = '➜',
-      deleted = '',
-      untracked = 'U',
-      ignored = '◌',
-    },
-    folder = {
-      default = '',
-      open = '',
-      empty = '',
-      empty_open = '',
-      symlink = '',
-    },
+    exclude = {},
   },
 }
 
 local M = {}
 
 M.setup = function()
-  nvimtree.setup(default)
+  nvimtree.setup(config)
 end
 
 return M
