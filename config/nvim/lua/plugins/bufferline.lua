f3fbb107cfb342a9610f4bf76b8cc09b5e15571d
Dorian Karter <dkarter@users.noreply.github.com>
2022-05-04 22:20:31 -0500

    style(nvim): remove buf numbers from bufferline

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -6,15 +6,16 @@ end
 
 local default = {
   options = {
-    numbers = 'ordinal',
     diagnostics = 'nvim_lsp',
     always_show_bufferline = false,
     show_close_icon = false,
-    offsets = { {
-      filetype = 'NvimTree',
-      text = 'File Explorer',
-      text_align = 'center',
-    } },
+    offsets = {
+      {
+        filetype = 'NvimTree',
+        text = 'File Explorer',
+        text_align = 'center',
+      },
+    },
   },
 }
 
