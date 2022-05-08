a60880daba441fb2aae6842a850850b1dd60c6e8
Dorian Karter <dkarter@users.noreply.github.com>
2022-05-08 17:55:39 -0500

    feat(nvim): add octo.nvim plugin for PR reviews

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -102,6 +102,18 @@ return packer.startup(function(use)
     end,
   }
 
+  use {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup()
+    end,
+  }
+
   -- " Visual git gutter (also used by feline)
   use {
     'lewis6991/gitsigns.nvim',
