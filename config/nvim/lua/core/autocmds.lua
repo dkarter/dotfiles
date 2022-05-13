64c6e860c445fa3423bb723668b9747a2c43b1c7
Dorian Karter <dkarter@users.noreply.github.com>
2022-05-12 21:39:54 -0500

    fix(nvim): add augroup for aucmd

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -1,7 +1,9 @@
 -- put event listeners here
 
--- spell check markdown and txt
-vim.api.nvim_create_autocmd(
-  { 'BufRead', 'BufNewFile' },
-  { pattern = { '*.txt', '*.md', '*.tex' }, command = 'setlocal spell' }
-)
+-- automatic spell check for some file types
+local spell_group = vim.api.nvim_create_augroup('SetSpell', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.txt', '*.md', '*.tex' },
+  command = 'setlocal spell',
+  group = spell_group,
+})
