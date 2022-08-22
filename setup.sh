b4530b8d0070d4817e701a47ec5966df7e0f9023
Dorian Karter <dkarter@users.noreply.github.com>
2022-08-21 22:30:33 -0500

    refactor(format): autoformat setup.sh

--- setup.sh
+++ setup.sh
@@ -4,33 +4,33 @@
 # Shamelessly copied from stackoverflow:
 # https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
 # =================================================================================
-lowercase(){
-    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+lowercase() {
+	echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
 }
 
-OS=`lowercase \`uname\``
+OS=$(lowercase $(uname))
 KERNEL="$(uname -r)"
 MACH="$(uname -m)"
 
 if [ "${OS}" = "darwin" ]; then
-    OS='mac'
+	OS='mac'
 else
-    OS="$(uname)"
-    if [ "${OS}" = "Linux" ] ; then
-        if [ -f /etc/debian_version ] ; then
-            DISTRO_BASE='debian'
-            DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
-        fi
-        if [ -f /etc/UnitedLinux-release ] ; then
-            DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
-        fi
-        OS=`lowercase $OS`
-        readonly OS
-        readonly DIST
-        readonly DISTRO_BASE
-        readonly KERNEL
-        readonly MACH
-    fi
+	OS="$(uname)"
+	if [ "${OS}" = "Linux" ]; then
+		if [ -f /etc/debian_version ]; then
+			DISTRO_BASE='debian'
+			DIST=$(cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F= '{ print $2 }')
+		fi
+		if [ -f /etc/UnitedLinux-release ]; then
+			DIST="${DIST}[$(cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//)]"
+		fi
+		OS=$(lowercase $OS)
+		readonly OS
+		readonly DIST
+		readonly DISTRO_BASE
+		readonly KERNEL
+		readonly MACH
+	fi
 
 fi
 
@@ -44,22 +44,20 @@ echo "$MACH"
 echo "==========================================="
 echo
 
-if [[ $DISTRO_BASE = 'debian' ]] ; then
-  echo "Debian based repo detected, getting required packages..."
-  ./installer/debian-setup.sh
-elif [[ $OS = 'mac' ]] ; then
-  echo 'Mac detected'
+if [[ $DISTRO_BASE = 'debian' ]]; then
+	echo "Debian based repo detected, getting required packages..."
+	./installer/debian-setup.sh
+elif [[ $OS = 'mac' ]]; then
+	echo 'Mac detected'
 
-  if ! command -v brew &> /dev/null; then
-    echo 'Homebrew not installed, installing...'
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-  fi
+	if ! command -v brew &>/dev/null; then
+		echo 'Homebrew not installed, installing...'
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+	fi
 
-  echo 'getting brew packges...'
-  brew bundle
+	echo 'getting brew packages...'
+	brew bundle
 fi
 
 ruby installer.rb
-
-
