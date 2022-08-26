746854e4dcb69c96bc23e7d7e02f42864be9f8c1
Dorian Karter <dkarter@users.noreply.github.com>
2022-08-25 21:14:19 -0500

    style(nvim): hide bufferline close icons

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -9,6 +9,7 @@ local default = {
     diagnostics = 'nvim_lsp',
     always_show_bufferline = false,
     show_close_icon = false,
+    show_buffer_close_icons = false,
     offsets = {
       {
         filetype = 'NvimTree',
