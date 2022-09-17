297426f3ee35b4b88f473b5e26d307dcf1e6c45a
Dorian Karter <dkarter@users.noreply.github.com>
2022-09-17 17:54:30 -0500

    fix(nvim): bring back vim-elixir for now
    Detect elixir syntax (should not be necessary in the future, but for now
    seems like it's required to detect eex files)

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -345,6 +345,10 @@ return packer.startup(function(use)
   --  }}}
 
   -- Elixir: {{{
+  -- Detect elixir syntax (should not be necessary in the future, but for now seems
+  -- like it's required to detect eex files)
+  use 'elixir-editors/vim-elixir'
+
   -- elixir text objects
   use {
     'kevinkoltz/vim-textobj-elixir',
