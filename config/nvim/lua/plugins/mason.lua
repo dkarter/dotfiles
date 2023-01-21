5229877aaa7f7ee0bfaa699f9aaed2184de948f8
Dorian Karter <dkarter@users.noreply.github.com>
2023-01-20 21:55:14 -0600

    fix(nvim): make tailwindcss work with Elixir HEEX templates

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -21,11 +21,87 @@ local M = {}
 
 -- Sets up Mason and Mason LSP Config
 M.setup = function()
+  local root_pattern = lspconfig.util.root_pattern
+
+  ---@diagnostic disable-next-line: redundant-parameter
   mason.setup {}
   mason_lspconfig.setup {}
   mason_lspconfig.setup_handlers {
     function(server_name)
       lspconfig[server_name].setup {}
+    end,
+
+    ['tailwindcss'] = function()
+      lspconfig.tailwindcss.setup {
+        root_dir = root_pattern(
+          'assets/tailwind.config.js',
+          'tailwind.config.js',
+          'tailwind.config.ts',
+          'postcss.config.js',
+          'postcss.config.ts',
+          'package.json',
+          'node_modules'
+        ),
+        init_options = {
+          userLanguages = {
+            elixir = 'phoenix-heex',
+            eruby = 'erb',
+            heex = 'phoenix-heex',
+            svelte = 'html',
+          },
+        },
+        handlers = {
+          ['tailwindcss/getConfiguration'] = function(_, _, params, _, bufnr, _)
+            vim.lsp.buf_notify(bufnr, 'tailwindcss/getConfigurationResponse', { _id = params._id })
+          end,
+        },
+        settings = {
+          includeLanguages = {
+            typescript = 'javascript',
+            typescriptreact = 'javascript',
+            ['html-eex'] = 'html',
+            ['phoenix-heex'] = 'html',
+            heex = 'html',
+            eelixir = 'html',
+            elm = 'html',
+            erb = 'html',
+            svelte = 'html',
+          },
+          tailwindCSS = {
+            lint = {
+              cssConflict = 'warning',
+              invalidApply = 'error',
+              invalidConfigPath = 'error',
+              invalidScreen = 'error',
+              invalidTailwindDirective = 'error',
+              invalidVariant = 'error',
+              recommendedVariantOrder = 'warning',
+            },
+            experimental = {
+              classRegex = {
+                [[class= "([^"]*)]],
+                [[class: "([^"]*)]],
+                '~H""".*class="([^"]*)".*"""',
+              },
+            },
+            validate = true,
+          },
+        },
+        filetypes = {
+          'css',
+          'scss',
+          'sass',
+          'html',
+          'heex',
+          'elixir',
+          'eruby',
+          'javascript',
+          'javascriptreact',
+          'typescript',
+          'typescriptreact',
+          'svelte',
+        },
+      }
     end,
 
     -- JSON
