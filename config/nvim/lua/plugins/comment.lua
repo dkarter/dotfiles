2a27e2e523546afab44c22cc9743601ae8aa96cc
Dorian Karter <dkarter@users.noreply.github.com>
2023-03-26 14:25:08 -0500

    fix(nvim): fix behavior of ts_context_commentstring

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -8,23 +8,7 @@ local M = {}
 
 M.setup = function()
   comment.setup {
-    pre_hook = function(ctx)
-      local U = require 'Comment.utils'
-      local utils = require 'ts_context_commentstring.utils'
-      local internal = require 'ts_context_commentstring.internal'
-
-      local location = nil
-      if ctx.ctype == U.ctype.block then
-        location = utils.get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = utils.get_visual_start_location()
-      end
-
-      return internal.calculate_commentstring {
-        key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
-        location = location,
-      }
-    end,
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
   }
 end
 
