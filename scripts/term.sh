e0e0f8e68a0c4a76637d25b1914803a6ad259078
Dorian Karter <dkarter@users.noreply.github.com>
2023-03-27 00:00:06 -0500

    feat(term,tmux): add script for setting up terminfo

--- scripts/term.sh
+++ scripts/term.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# update terminal
-tic "$HOME/dotfiles/iterm/xterm-256color-italic.terminfo"
-tic "$HOME/dotfiles/iterm/screen-256color-italic.terminfo"
-tic "$HOME/dotfiles/iterm/xterm-256color-italic.ti"
+set -euxo pipefail
+
+# download an updated terminfo file
+mkdir /tmp/terminfo
+pushd /tmp/terminfo
+curl -LO https://invisible-island.net/datafiles/current/terminfo.src.gz && gunzip terminfo.src.gz
+tic -xe alacritty-direct,tmux-256color terminfo.src
+popd
