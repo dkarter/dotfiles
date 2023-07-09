3da2331851e2b260e11dd4fcfc5b2a1b833f62c1
Dorian Karter <dkarter@users.noreply.github.com>
2023-07-09 15:35:24 -0500

    chore(zsh): delete fgstash
    Unused and was not working properly

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -86,34 +86,6 @@ fgcs() {
     echo -n $(echo "$commit" | sed "s/ .*//")
 }
 
-# fgstash - easier way to deal with stashes
-# type fstash to get a list of your stashes
-# enter shows you the contents of the stash
-# ctrl-d shows a diff of the stash against your current HEAD
-# ctrl-b checks the stash out as a branch, for easier merging
-fgstash() {
-  local out q k sha
-  while out=$(
-    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-      fzf --ansi --no-sort --query="$q" --print-query \
-        --expect=ctrl-d,ctrl-b
-  ); do
-    mapfile -t out <<<"$out"
-    q="${out[0]}"
-    k="${out[1]}"
-    sha="${out[-1]}"
-    sha="${sha%% *}"
-    [[ -z "$sha" ]] && continue
-    if [[ "$k" == 'ctrl-d' ]]; then
-      git diff $sha
-    elif [[ "$k" == 'ctrl-b' ]]; then
-      git stash branch "stash-$sha" $sha
-      break
-    else
-      git stash show -p $sha
-    fi
-  done
-}
 
 # Install one or more versions of specified language
 # e.g. `vmi rust` # => fzf multimode, tab to mark, enter to install
