a6211f28396eb7ca1daba12e1435264489f9cb55
Dorian Karter <dkarter@users.noreply.github.com>
2023-07-17 15:29:40 -0500

    fix(zsh): prevent conflict between teleport and 1password cli

--- config/zsh/op.zsh
+++ config/zsh/op.zsh
@@ -3,3 +3,6 @@ compdef _op op
 
 # Enable SSH Agent for all hosts
 export SSH_AUTH_SOCK=~/.1password/agent.sock
+
+# prevent an issue with TELEPORT conflicting with 1password agent
+export TELEPORT_ADD_KEYS_TO_AGENT=no
