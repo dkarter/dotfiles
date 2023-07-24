5184155213abe747c1cdd335d5970da5baac5c7e
Dorian Karter <dkarter@users.noreply.github.com>
2023-07-23 23:32:15 -0500

    refactor(nvim): restructure flash mappings for readability

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -238,46 +238,11 @@ M.todo_comments_mappings = {
 
 -- stylua: ignore
 M.flash_mappings = {
-  {
-    '<leader>hp',
-    mode = { 'n', 'x', 'o' },
-    function()
-      require('flash').jump()
-    end,
-    desc = 'Flash',
-  },
-  {
-    '<leader>fs',
-    mode = { 'n', 'o', 'x' },
-    function()
-      require('flash').treesitter()
-    end,
-    desc = 'Flash Treesitter',
-  },
-  {
-    'r',
-    mode = 'o',
-    function()
-      require('flash').remote()
-    end,
-    desc = 'Remote Flash',
-  },
-  {
-    'R',
-    mode = { 'o', 'x' },
-    function()
-      require('flash').treesitter_search()
-    end,
-    desc = 'Flash Treesitter Search',
-  },
-  {
-    '<c-s>',
-    mode = { 'c' },
-    function()
-      require('flash').toggle()
-    end,
-    desc = 'Toggle Flash Search',
-  },
+  { '<leader>hp', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash', },
+  { '<leader>fs', mode = { 'n', 'o', 'x' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter', },
+  { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash', },
+  { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Flash Treesitter Search', },
+  { '<c-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search', },
 }
 
 M.nvim_tree_mappings = {
