e22d17189a7b9da7879a09d8be88f506de17f464
Dorian Karter <dkarter@users.noreply.github.com>
2023-07-23 23:30:01 -0500

    feat(nvim): add [q ]q for jumping between Trouble listings
    With fallback for previous behavior if trouble is closed

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -195,6 +195,34 @@ M.trouble_mappings = {
   { '<leader>xd', '<cmd>Trouble document_diagnostics<cr>', desc = 'Document Diagnostics' },
   { '<leader>xl', '<cmd>Trouble loclist<cr>', desc = 'Open Loclist' },
   { '<leader>xq', '<cmd>Trouble quickfix<cr>', desc = 'Open Quickfix' },
+  {
+    '[q',
+    function()
+      if require('trouble').is_open() then
+        require('trouble').previous { skip_groups = true, jump = true }
+      else
+        local ok, err = pcall(vim.cmd.cprev)
+        if not ok then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end
+    end,
+    desc = 'Previous trouble/quickfix item',
+  },
+  {
+    ']q',
+    function()
+      if require('trouble').is_open() then
+        require('trouble').next { skip_groups = true, jump = true }
+      else
+        local ok, err = pcall(vim.cmd.cnext)
+        if not ok then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end
+    end,
+    desc = 'Next trouble/quickfix item',
+  },
   { 'gR', '<cmd>Trouble lsp_references<cr>', desc = '[G]o to [R]eferences (Trouble)' },
 }
 
