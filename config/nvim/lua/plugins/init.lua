5f155dcd1374331bd40238cade4522c00ace128c
Dorian Karter <dkarter@users.noreply.github.com>
2023-07-23 23:41:18 -0500

    feat(nvim): add illuminate to highlight word under cursor

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -90,6 +90,25 @@ require('lazy').setup({
     end,
   },
 
+  -- Automatically highlights other instances of the word under your cursor.
+  -- This works with LSP, Treesitter, and regexp matching to find the other
+  -- instances.
+  {
+    'RRethy/vim-illuminate',
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {
+      delay = 200,
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { 'lsp' },
+      },
+    },
+    -- this is a good example of how to have both `opts` and `config`
+    config = function(_, opts)
+      require('illuminate').configure(opts)
+    end,
+  },
+
   -- jump anywhere
   {
     'folke/flash.nvim',
