019f7674829b43cef103e5b0681b4e43b041f52e
Dorian Karter <dkarter@users.noreply.github.com>
2023-07-31 22:32:33 -0500

    fix(nvim): auto load treesitter when loading telescope
    Telescope uses treesitter in previews

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -661,6 +661,8 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
       'folke/tokyonight.nvim',
       'nvim-telescope/telescope-symbols.nvim',
+      -- Telescope uses treesitter for previews
+      'nvim-treesitter/nvim-treesitter',
     },
     config = function()
       require('plugins.telescope').setup()
