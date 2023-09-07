966b191e6507b52c0eebfeb4ff5d4896f976a44b
Dorian Karter <dkarter@users.noreply.github.com>
2023-09-06 22:00:49 -0500

    fix(zsh): add yarn bin folder to PATH on linux

--- config/zsh/yarn.zsh
+++ config/zsh/yarn.zsh
@@ -1,3 +1,4 @@
 # set yarn binaries on path
 export PATH="$(yarn global bin):$PATH"
 export PATH="$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+export PATH="$HOME/.local/share/yarn/global/node_modules/.bin:$PATH"
