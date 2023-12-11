cd0c644f29d28e506e8944410cffbb62c23181b4
Dorian Karter <dkarter@users.noreply.github.com>
2023-12-11 09:55:29 -0600

    feat(wezterm): add mapping to toggle opacity+blur

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,24 +1,50 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 20
+config.font_size = 18
+
+local font_family = 'CaskaydiaCove Nerd Font Mono'
 
 -- wezterm comes with JetBrains Mono and a default fallback font for symbols
 -- so technically, this is not needed. But the fallback symbols are a bit odd
 -- looking compared to JetBrains Mono (seem too big or stretched out)
 config.font = wezterm.font_with_fallback {
-  { family = 'CaskaydiaCove Nerd Font Mono', weight = 'Regular', stretch = 'Normal', style = 'Normal' },
-  { family = 'CaskaydiaCove Nerd Font Mono', weight = 'Regular', stretch = 'Normal', style = 'Italic' },
-  { family = 'CaskaydiaCove Nerd Font Mono', weight = 'Bold', stretch = 'Normal', style = 'Normal' },
-  { family = 'CaskaydiaCove Nerd Font Mono', weight = 'Bold', stretch = 'Normal', style = 'Italic' },
+  { family = font_family, weight = 'Regular', stretch = 'Normal', style = 'Normal' },
+  { family = font_family, weight = 'Regular', stretch = 'Normal', style = 'Italic' },
+  { family = font_family, weight = 'Bold', stretch = 'Normal', style = 'Normal' },
+  { family = font_family, weight = 'Bold', stretch = 'Normal', style = 'Italic' },
 }
+
+config.color_scheme = 'tokyonight'
+
+-- that sweet dark background
+config.colors = {
+  background = '#000000',
+}
+
+wezterm.on('toggle-opacity', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.window_background_opacity then
+    overrides.window_background_opacity = 0.8
+    overrides.macos_window_background_blur = 20
+  else
+    overrides.window_background_opacity = nil
+    overrides.macos_window_background_blur = nil
+  end
+  window:set_config_overrides(overrides)
+end)
 
 -- disable tab bar (tabs are handled by tmux)
 config.enable_tab_bar = false
 
+-- if enabling this, check https://github.com/stepanzak/dotfiles/blob/main/dot_config/wezterm/wezterm.lua
 -- config.disable_default_key_bindings = true
 config.keys = {
+  {
+    key = 'O',
+    mods = 'SUPER|SHIFT',
+    action = wezterm.action.EmitEvent 'toggle-opacity',
+  },
   {
     key = 'P',
     mods = 'SUPER|SHIFT',
@@ -84,15 +110,6 @@ config.keys = {
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A9',
   },
-}
-
-config.font_size = 18
-
-config.color_scheme = 'tokyonight'
-
--- that sweet dark background
-config.colors = {
-  background = '#000000',
 }
 
 return config
