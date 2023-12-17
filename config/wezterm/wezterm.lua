69e20e629ff79b77d39f3656d74777aa76e1276d
Dorian Karter <dkarter@users.noreply.github.com>
2023-12-17 17:37:58 -0600

    feat(tmux): add tmux session manager

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,3 +1,13 @@
+--
+-- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
+-- ██║    ██║██╔════╝╚══███╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
+-- ██║ █╗ ██║█████╗    ███╔╝    ██║   █████╗  ██████╔╝██╔████╔██║
+-- ██║███╗██║██╔══╝   ███╔╝     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║
+-- ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║
+--  ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝
+-- A GPU-accelerated cross-platform terminal emulator
+-- https://wezfurlong.org/wezterm/
+
 local wezterm = require 'wezterm'
 local config = {}
 
@@ -45,66 +55,87 @@ config.keys = {
     mods = 'SUPER|SHIFT',
     action = wezterm.action.EmitEvent 'toggle-opacity',
   },
-  {
-    key = 'P',
-    mods = 'SUPER|SHIFT',
-    action = wezterm.action.SendString '\x1AP',
-  },
+
   {
     key = 'T',
     mods = 'SUPER|SHIFT',
     action = wezterm.action.SendString '\x1Ac',
   },
+
+  -- jump windows
+  {
+    key = 'k',
+    mods = 'SUPER',
+    action = wezterm.action.SendString '\x1AP',
+  },
+
+  -- open t - tmux smart session manager
+  {
+    key = 'j',
+    mods = 'SUPER',
+    action = wezterm.action.SendString '\x1AT',
+  },
+
   {
     key = '[',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1Ap',
   },
+
   {
     key = ']',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1An',
   },
+
   {
     key = '1',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A1',
   },
+
   {
     key = '2',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A2',
   },
+
   {
     key = '3',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A3',
   },
+
   {
     key = '4',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A4',
   },
+
   {
     key = '5',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A5',
   },
+
   {
     key = '6',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A6',
   },
+
   {
     key = '7',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A7',
   },
+
   {
     key = '8',
     mods = 'SUPER',
     action = wezterm.action.SendString '\x1A8',
   },
+
   {
     key = '9',
     mods = 'SUPER',
---
+++ config/zsh/tmux.zsh
@@ -0,0 +1,2 @@
+# enable session manager https://github.com/joshmedeski/t-smart-tmux-session-manager
+export PATH=$HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin:$PATH
