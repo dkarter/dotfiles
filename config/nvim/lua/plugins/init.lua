dc379d8a3862dbca88fd8b4b239aef9941e4a4ef
Dorian Karter <dkarter@users.noreply.github.com>
2024-01-17 10:15:24 -0600

    fix(nvim): gx.nvim mapping change for new version

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -452,7 +452,7 @@ require('lazy').setup({
   -- smarter gx mapping
   {
     'chrishrb/gx.nvim',
-    keys = { 'gx' },
+    keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
       handler_options = {
