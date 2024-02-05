8691cc6e3952ef4fc79c1f81108c4f7f5e2653aa
Dorian Karter <dkarter@users.noreply.github.com>
2024-02-05 11:18:02 -0600

    build(nvim): update plugins

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -469,6 +469,7 @@ require('lazy').setup({
     'chrishrb/gx.nvim',
     keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
     dependencies = { 'nvim-lua/plenary.nvim' },
+    submodules = false,
     opts = {
       handler_options = {
         search_engine = 'duckduckgo',
