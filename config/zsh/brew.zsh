d9e594f242aab19849a581bae7d4814ff95acd55
Dorian Karter <dkarter@users.noreply.github.com>
2024-06-13 01:53:33 -0500

    fix(zsh): put brew path at the end
    To allow asdf to take precedence

--- config/zsh/brew.zsh
+++ config/zsh/brew.zsh
@@ -3,6 +3,6 @@ export HOMEBREW_GITHUB_API_TOKEN="<<-[[CHANGEINLOCALZSHRC]]->>"
 
 
 # set homebrew on path (Intel)
-export PATH="/usr/local/bin:$PATH"
+export PATH="$PATH:/usr/local/bin"
 # set homebrew on path (Apple Silicon)
-export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$PATH:/opt/homebrew/bin"
