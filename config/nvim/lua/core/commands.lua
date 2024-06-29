8e0a1b7fd49fb8e9079a16730b5680af621741de
Dorian Karter <dkarter@users.noreply.github.com>
2024-06-28 22:21:22 -0500

    feat(nvim): improve Cfd and Cfd!

--- config/nvim/lua/core/commands.lua
+++ config/nvim/lua/core/commands.lua
@@ -12,33 +12,47 @@ cmd('CopyFileName', "let @+=expand('%:t')", {})
 
 cmd('ReloadModules', utils.reload_modules, {})
 
-local function grepFileNames(opts)
-  local bang = opts.bang or false
-  local args = opts.args or ''
-  local grepprg = vim.api.nvim_get_option 'grepprg'
-  local grepformat = vim.api.nvim_get_option 'grepformat'
-  local shellpipe = vim.api.nvim_get_option 'shellpipe'
+--- Function to search files using fd and send results to the quickfix list
+--- Powers :Cfd
+---@param input table command input
+local function search_with_fd(input)
+  local args = { '--regex', '--hidden', '--type file' }
 
-  vim.api.nvim_set_option('grepprg', 'fd')
-  vim.api.nvim_set_option('grepformat', '%f')
-
-  if shellpipe == '2>&1| tee' or shellpipe == '|& tee' then
-    vim.api.nvim_set_option('shellpipe', '| tee')
+  -- search on the full path if the command was banged
+  if input.bang then
+    table.insert(args, '--full-path')
   end
 
-  vim.api.nvim_command('silent grep! ' .. args)
+  local cmd = 'fd ' .. table.concat(args, ' ') .. ' ' .. input.args
 
-  if bang == false and not vim.fn.empty(vim.fn.getqflist()) then
-    vim.schedule(function()
-      vim.api.nvim_command 'cfirst'
-    end)
+  -- Run the fd command
+  local handle = io.popen(cmd)
+
+  -- a nil check for handle (linter told me to, not sure how this happens)
+  if not handle then
+    return
   end
 
-  vim.api.nvim_set_option('grepprg', grepprg)
-  vim.api.nvim_set_option('grepformat', grepformat)
-  vim.api.nvim_set_option('shellpipe', shellpipe)
+  local result = handle:read '*a'
+  handle:close()
+
+  -- Split the result into lines
+  local lines = {}
+  for line in result:gmatch '[^\r\n]+' do
+    table.insert(lines, { filename = line, lnum = 1, col = 1, text = line })
+  end
+
+  if vim.tbl_isempty(lines) then
+    vim.notify 'No results'
+  else
+    vim.fn.setqflist({}, 'r', { title = 'fd search results', items = lines })
+    vim.cmd 'cfirst'
+    vim.cmd 'copen'
+  end
 end
 
 -- searches for files matching the regex pattern and dumps the result in the
 -- quickfix list for easy navigation
-cmd('Cfd', grepFileNames, { nargs = '+', complete = 'file', bang = true })
+-- When the command is banged (`:Cfd! something`), then it will search on the
+-- full path, not just the file name
+cmd('Cfd', search_with_fd, { nargs = '+', complete = 'file', bang = true })
