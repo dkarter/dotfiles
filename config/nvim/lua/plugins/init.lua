84e3c4a1cff0d426a291f83529e7ed3cf05831c6
Dorian Karter <dkarter@users.noreply.github.com>
2024-07-02 23:46:12 -0500

    fix(nvim): correctly set color scheme

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -307,20 +307,38 @@ require('lazy').setup({
       style = 'moon',
       transparent = true,
     },
+    config = function()
+      vim.cmd.colorscheme 'tokyonight-moon'
+    end,
   },
 
   -- automatically set color scheme based on system
   {
     'f-person/auto-dark-mode.nvim',
+    event = 'VeryLazy',
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value('background', 'dark', { scope = 'global' })
-        vim.cmd.colorscheme 'tokyonight'
+
+        vim.cmd.colorscheme 'tokyonight-moon'
+
+        -- only change theme on lualine if it's loaded
+        if package.loaded['lualine'] then
+          ---@diagnostic disable-next-line: redundant-parameter
+          require('lualine').setup { options = { theme = 'tokyonight-moon' } }
+        end
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value('background', 'light', { scope = 'global' })
+
         vim.cmd.colorscheme 'tokyonight-day'
+
+        -- only change theme on lualine if it's loaded
+        if package.loaded['lualine'] then
+          ---@diagnostic disable-next-line: redundant-parameter
+          require('lualine').setup { options = { theme = 'tokyonight-day' } }
+        end
       end,
     },
   },
