2a2b975f34e3d1b5a7b1b52808fe9044b03e57aa
Dorian Karter <dkarter@users.noreply.github.com>
2024-07-03 16:28:57 -0500

    fix(zsh): enable fzf shell integration

--- config/zsh/fzf.zsh
+++ config/zsh/fzf.zsh
@@ -12,3 +12,7 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
   --color=fg+:#c5cdd9,bg+:#000000,hl+:#5dbbc1
   --color=info:#88909f,prompt:#ec7279,pointer:#d38aea
   --color=marker:#a0c980,spinner:#ec7279,header:#5dbbc1'
+
+# enable shell integration
+source <(fzf --zsh)
+
