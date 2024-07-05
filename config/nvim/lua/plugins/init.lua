b50564e67acad9a0b58d20c8091fecf8d4578fc1
Dorian Karter <dkarter@users.noreply.github.com>
2024-07-05 01:31:58 -0500

    refactor(nvim): init tokyonight the config way

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -306,11 +306,11 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     lazy = false, -- make sure we load this during startup
     priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      style = 'moon',
-      transparent = true,
-    },
     config = function()
+      require('tokyonight').setup {
+        style = 'moon',
+        transparent = true,
+      }
       vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
