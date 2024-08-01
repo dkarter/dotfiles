c2143dee6ca1f0e8c7e3ed4abad62fb4ca2df4b3
Dorian Karter <dkarter@users.noreply.github.com>
2024-08-01 00:52:08 -0500

    refactor(nvim): use coerce.nvim for case coercion

---
+++ config/nvim/lua/plugins/coerce.lua
@@ -0,0 +1,8 @@
+-- change keyword case (camel case etc), supports LSP rename, which key,
+-- motions, etc
+---@type LazySpec
+return {
+  'gregorias/coerce.nvim',
+  keys = { 'cr', 'gcr' },
+  opts = {},
+}
--- config/nvim/lua/plugins/vim-abolish.lua
+++ config/nvim/lua/plugins/vim-abolish.lua
@@ -1,4 +1,11 @@
 -- abolish.vim: easily search for, substitute, and abbreviate multiple variants
 -- of a word
 ---@type LazySpec
-return { 'tpope/vim-abolish', event = { 'BufReadPost', 'BufNewFile' } }
+return {
+  'tpope/vim-abolish',
+  cmd = { 'Abolish', 'Subvert', 'S' },
+  init = function()
+    -- mappings for coercion will be used by coerce.nvim
+    vim.g.abolish_no_mappings = 1
+  end,
+}
