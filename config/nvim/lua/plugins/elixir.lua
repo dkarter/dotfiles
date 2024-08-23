b972b05f7de468290df2185159edb9019184add7
Dorian Karter <dkarter@users.noreply.github.com>
2024-08-23 16:44:59 -0500

    feat(nvim): add nextls config (disabled)
    Makes it easy to enable if ElixirLS starts misbehaving

--- config/nvim/lua/plugins/elixir.lua
+++ config/nvim/lua/plugins/elixir.lua
@@ -9,7 +9,24 @@ return {
     local elixirls = require 'elixir.elixirls'
 
     elixir.setup {
-      nextls = { enable = false },
+      nextls = {
+        enable = false,
+        -- installed via mason
+        cmd = 'nextls',
+        init_options = {
+          mix_env = 'dev',
+          mix_target = 'host',
+          experimental = {
+            completions = {
+              enable = true,
+            },
+          },
+        },
+        on_attach = function(client, bufnr)
+          require('core.mappings').elixir_mappings()
+          require('plugins.mason.lsp').on_attach(client, bufnr)
+        end,
+      },
       credo = { enable = true },
       elixirls = {
         enable = true,
