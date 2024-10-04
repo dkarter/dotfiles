7d1b6cefcee00b198551e6bb4ad6b1e57ac4d619
Dorian Karter <dkarter@users.noreply.github.com>
2024-10-04 01:53:47 -0500

    feat(brew,zsh): add homebrew support for linux

--- config/zsh/brew.zsh
+++ config/zsh/brew.zsh
@@ -1,6 +1,12 @@
 # HomeBrew Github API Token Placeholder
 export HOMEBREW_GITHUB_API_TOKEN="<<-[[CHANGEINLOCALZSHRC]]->>"
 
+if [ -d /home/linuxbrew ]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+# disable analytics
+export HOMEBREW_NO_ANALYTICS=1
 
 # set homebrew on path (Intel)
 export PATH="$PATH:/usr/local/bin"
