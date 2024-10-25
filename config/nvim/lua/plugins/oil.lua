84e22ddc1060d8ee2e5dfb96bbe788393707551b
Dorian Karter <dkarter@users.noreply.github.com>
2024-10-25 18:16:24 -0500

    feat(nvim): improve oil.nvim config
    https://www.youtube.com/watch?v=q1QhV-24DNA

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -6,14 +6,24 @@ return {
   -- it's ok though because this only adds ~1.5ms to load time
   event = { 'VimEnter' },
   keys = require('core.mappings').oil_nvim_mappings,
+  ---@module "oil"
+  ---@class oil.Config
   opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
     columns = { 'icon' },
     keymaps = {
       ['<C-h>'] = false,
       ['<S-CR>'] = 'actions.select_split',
     },
+    ---@diagnostic disable-next-line: missing-fields
     view_options = {
       show_hidden = true,
+      natural_order = true,
+      is_always_hidden = function(name, _bufnr)
+        return name == '..' or name == '.git'
+      end,
     },
   },
   -- Optional dependencies
