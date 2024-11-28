a3379901dbeb203fcf65ec1cb2350337bb9737e6
Dorian Karter <dkarter@users.noreply.github.com>
2024-11-27 22:23:50 -0600

    refactor(zsh): improve completions

--- config/zsh/completions.zsh
+++ config/zsh/completions.zsh
@@ -1,5 +1,20 @@
+# fzf-tab and completion settings
+# -------------------------------
 # case insensitive autocomplete
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+# preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
+# --------------------------------
 
 # red dots to be displayed while waiting for completion
 export COMPLETION_WAITING_DOTS="true"
@@ -26,7 +41,6 @@ done
 compinit -C
 
 # requires brew install carapace
-export CARAPACE_BRIDGES='zsh,bash,inshellisense' # optional
-zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense'
 source <(carapace _carapace)
 
