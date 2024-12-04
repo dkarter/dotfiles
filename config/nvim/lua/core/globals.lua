a31255dcd559098987b28e38122adee77693ba33
Dorian Karter <dkarter@users.noreply.github.com>
2024-12-04 16:42:17 -0600

    feat(nvim): add better lua debugging utils via Snacks

--- config/nvim/lua/core/globals.lua
+++ config/nvim/lua/core/globals.lua
@@ -1,5 +1,14 @@
+---@diagnostic disable: undefined-global
 _G.I = vim.inspect
 _G.fmt = string.format
-_G.dbg = function(thing)
-  vim.notify(I(thing))
+_G.dbg = function(...)
+  Snacks.debug.inspect(...)
 end
+
+_G.dd = function(...)
+  Snacks.debug.inspect(...)
+end
+_G.bt = function()
+  Snacks.debug.backtrace()
+end
+vim.print = _G.dd
