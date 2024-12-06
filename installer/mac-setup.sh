0aeba562fb3c471373192623432beada0885585b
Dorian Karter <dkarter@users.noreply.github.com>
2024-12-06 12:59:36 -0600

    refactor(task,setup): move 1password symlink to task

--- installer/mac-setup.sh
+++ installer/mac-setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# symlink 1password ssh agent sock file so that the paths are compatible between
-# mac and linux (linux uses the ~/.1password, and mac uses that ugly path below)
-if [ ! -d "$HOME/.1password" ]; then
-  echo 'Symlinking 1Password SSH Agent Sock'
-  mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
-fi
-
 if [ "$(hidutil property --get 'UserKeyMapping')" == '(null)' ]; then
   echo 'Remapping macOS modifiers...'
   # remap CapsLock to Escape (now)
