df668d6e24659a2c27b4427fae915bfa811b3ca8
Dorian Karter <dkarter@users.noreply.github.com>
2024-12-07 00:50:15 -0600

    fix(nvim): disable treesitter for large files

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -81,7 +81,17 @@ return {
         'zig',
       },
       ---@type TSModule
-      highlight = { enable = true },
+      highlight = {
+        enable = true,
+        -- disable treesitter highlighting for really large files
+        disable = function(_lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
       ---@type TSModule
       indent = {
         enable = true,
