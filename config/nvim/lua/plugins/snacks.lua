12a169d6de391af552922521bf2a1d8a39301456
Dorian Karter <dkarter@users.noreply.github.com>
2024-12-16 12:59:06 -0600

    feat(nvim): notify LSP on file rename from nvim-tree

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -22,6 +22,21 @@ return {
     end
 
     vim.print = _G.dd
+
+    -- tell LSPs when a file has been renamed (via NvimTree)
+    local prev = { new_name = '', old_name = '' } -- Prevents duplicate events
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'NvimTreeSetup',
+      callback = function()
+        local events = require('nvim-tree.api').events
+        events.subscribe(events.Event.NodeRenamed, function(data)
+          if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
+            data = data
+            Snacks.rename.on_rename_file(data.old_name, data.new_name)
+          end
+        end)
+      end,
+    })
   end,
   ---@module "snacks"
   ---@type snacks.Config
