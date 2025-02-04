2401bae707bade9d5dd39676baac47d7fb48419a
Dorian Karter <dkarter@users.noreply.github.com>
2025-02-03 22:03:20 -0600

    feat(nvim): add `q` mapping to exit fugitive blame

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -5,5 +5,18 @@ return {
   event = 'VeryLazy',
   config = function()
     require('core.mappings').fugitive_mappings()
+
+    local augroup = require('core.utils').augroup
+
+    augroup('FugitiveUser', {
+      -- add custom bindings for Git Blame buffer
+      {
+        event = { 'FileType' },
+        pattern = { 'fugitiveblame' },
+        command = function()
+          vim.keymap.set('n', 'q', ':q<CR>', { noremap = true, buffer = true, desc = 'Close Blame' })
+        end,
+      },
+    })
   end,
 }
