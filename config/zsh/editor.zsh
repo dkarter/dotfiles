6ab62e01ee80feae51d7bd3428812af2e96f9df2
Dorian Karter <dkarter@users.noreply.github.com>
2025-02-08 23:40:30 -0600

    fix(zsh): set EDITOR to nvim always

---
+++ config/zsh/editor.zsh
@@ -0,0 +1,4 @@
+export EDITOR="nvim"
+export VISUAL="nvim"
+export ELIXIR_EDITOR="nvim"
+export ECTO_EDITOR="nvim"
--- config/zsh/tmux.zsh
+++ config/zsh/tmux.zsh
@@ -5,27 +5,6 @@
 # https://github.com/trevarj/tmux-open-nvim#caveat
 export PATH="$PATH:$HOME/.config/tmux/plugins/tmux-open-nvim/scripts"
 
-# only run this inside tmux
-if [[ -n $TMUX ]]; then
-  # support for tmux open nvim
-  export EDITOR="ton"
-  export VISUAL="ton"
-
-  function e() {
-    ton "$1"
-  }
-
-  # Elixir Editor Support + tmux open nvim
-  # -------------------------------------------
-
-  # https://hexdocs.pm/iex/IEx.Helpers.html#open/1
-  export ELIXIR_EDITOR="ton"
-
-  # see https://hexdocs.pm/ecto/Mix.Tasks.Ecto.Gen.Repo.html
-  # and https://hexdocs.pm/ecto_sql/Mix.Tasks.Ecto.Gen.Migration.html
-  export ECTO_EDITOR="ton"
-fi
-
 # --------------------------------------------
 # END tmux-open-nvim
 # --------------------------------------------
