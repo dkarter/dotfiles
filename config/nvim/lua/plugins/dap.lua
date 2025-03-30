cf55a7ed6d90ee6ce2a3311692ae2f234e521a13
Dorian Karter <dkarter@users.noreply.github.com>
2025-03-30 01:22:25 -0500

    fix(nvim): remove DAP sign_define - deprecated

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -98,17 +98,6 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
-    -- stylua: ignore
-    local setup_gutter_signs = function()
-      vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-      vim.fn.sign_define('DapBreakpointCondition', { text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-      vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
-      vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
-      vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
-    end
-
-    setup_gutter_signs()
-
     -- automatically open DAP UI
     dap.listeners.before.attach.dapui_config = dapui.open
     dap.listeners.before.launch.dapui_config = dapui.open
