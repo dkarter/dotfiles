013df843c64b3007c29dc6af9a1939fee17167e2
Dorian Karter <dkarter@users.noreply.github.com>
2025-05-11 22:35:45 -0500

    refactor(nvim): switch to blink.cmp
    It's been some time, want to see if some of the problems have been
    solved now.

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -2,7 +2,7 @@
 return {
   'hrsh7th/nvim-cmp',
   -- this plugin will eventually be replaced with blink-cmp
-  enabled = true,
+  enabled = false,
   event = { 'CmdlineEnter', 'InsertEnter' },
   dependencies = {
     -- snippet engine, required by cmp
