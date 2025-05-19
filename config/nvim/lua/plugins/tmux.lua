bc5945d2e77ea31943c2e90dbb7bc581432591c0
Dorian Karter <dkarter@users.noreply.github.com>
2025-05-19 11:57:45 -0500

    fix(nvim): seamless navigation fix for Snacks explorer

--- config/nvim/lua/plugins/tmux.lua
+++ config/nvim/lua/plugins/tmux.lua
@@ -9,4 +9,22 @@ return {
       enable = false,
     },
   },
+  config = function(_self, opts)
+    require('tmux').setup(opts)
+    -- this is a stupid fix so seamless tmux navigation works with Snacks
+    -- explorer
+    require('tmux.wrapper.nvim').is_nvim_float = function()
+      if Snacks then
+        local is_explorer = vim
+          .iter(Snacks.picker.get { source = 'explorer' })
+          :any(function(picker) ---@param picker snacks.Picker
+            return picker:is_focused()
+          end)
+        if is_explorer then
+          return false
+        end
+      end
+      return vim.api.nvim_win_get_config(0).relative ~= ''
+    end
+  end,
 }
