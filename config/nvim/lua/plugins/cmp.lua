d5408ddad6d2145006a4d1cebef901f9c6b2e958
Dorian Karter <dkarter@users.noreply.github.com>
2025-05-21 22:17:04 -0500

    fix(nvim): remove cmp handlers for copilot

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -48,14 +48,6 @@ return {
       return false
     end
 
-    cmp.event:on('menu_opened', function()
-      vim.b.copilot_suggestion_hidden = true
-    end)
-
-    cmp.event:on('menu_closed', function()
-      vim.b.copilot_suggestion_hidden = false
-    end)
-
     local has_words_before = function()
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
