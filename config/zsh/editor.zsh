9f10951e524f60e82e673ea8b8442a96a479716b
Dorian Karter <dkarter@users.noreply.github.com>
2025-07-05 00:01:33 -0500

    fix(zsh): elixir editors need to be opened in another window

--- config/zsh/editor.zsh
+++ config/zsh/editor.zsh
@@ -1,4 +1,7 @@
 export EDITOR="nvim"
 export VISUAL="nvim"
-export ELIXIR_EDITOR="nvim"
-export ECTO_EDITOR="nvim"
+# this is a fix because elixir editors need to be opened in a separate window
+# see https://github.com/elixir-ecto/ecto/issues/1581#issuecomment-233126107
+export ELIXIR_EDITOR='tmux display-popup -E -w 80% -h 80% "nvim +__LINE__ __FILE__"'
+export PLUG_EDITOR="$ELIXIR_EDITOR"
+export ECTO_EDITOR="$ELIXIR_EDITOR"
