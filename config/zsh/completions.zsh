c7748ce167ceddea105655dcf7586503e648e997
Dorian Karter <dkarter@users.noreply.github.com>
2025-07-06 19:19:49 -0500

    refactor(mise): remove need to compile some deps

--- config/zsh/completions.zsh
+++ config/zsh/completions.zsh
@@ -10,7 +10,7 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 zstyle ':completion:*' menu no
 # preview directory's content with eza when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color=always --icon=always --group-directories-first $realpath'
 zstyle ':fzf-tab:*' use-fzf-default-opts yes
 # switch group using `<` and `>`
 zstyle ':fzf-tab:*' switch-group '<' '>'
