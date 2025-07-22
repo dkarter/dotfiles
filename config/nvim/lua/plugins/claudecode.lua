c5094435d16a2a2aaeac8dd024bdbdef28393e1f
Dorian Karter <dkarter@users.noreply.github.com>
2025-07-21 20:32:58 -0500

    fix(nvim): load claudecode on VeryLazy

--- config/nvim/lua/plugins/claudecode.lua
+++ config/nvim/lua/plugins/claudecode.lua
@@ -2,6 +2,9 @@
 ---@type LazySpec
 return {
   'coder/claudecode.nvim',
+  -- load on start (but delayed) to allow IDE integration from another tmux pane
+  -- (/ide)
+  event = 'VeryLazy',
   dependencies = { 'folke/snacks.nvim' },
   opts = {},
   keys = require('core.mappings').claudecode_mappings,
