c5b2b15b63717a96a7881226cbf7f1493cd47d4a
Dorian Karter <dkarter@users.noreply.github.com>
2025-08-07 12:35:42 -0500

    fix(nvim): attempt to disable folds in diffview

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -8,4 +8,13 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   keys = require('core.mappings').diffview_mappings,
+  opts = {
+    hooks = {
+      -- disable folding in diffview windows (they are annoying - if the the
+      -- file is too large it should probably be split anyways)
+      diff_buf_win_enter = function()
+        vim.opt_local.foldenable = false
+      end,
+    },
+  },
 }
