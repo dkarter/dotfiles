568ea3eff91f708f15e3cd5af18b6d9ec5cbe17c
Dorian Karter <dkarter@users.noreply.github.com>
2025-08-08 15:33:52 -0500

    feat(nvim): add ethersync plugin

---
+++ config/nvim/lua/plugins/ethersync.lua
@@ -0,0 +1,6 @@
+-- Pair Programming with Ethersync https://github.com/ethersync/ethersync
+---@type LazySpec
+return {
+  'ethersync/ethersync-nvim',
+  keys = { { '<leader>es', '<cmd>EthersyncJumpToCursor<cr>' } },
+}
