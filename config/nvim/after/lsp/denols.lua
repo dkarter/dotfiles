132e6dc7db2d789925624e361ff2bfdebeb4db9c
Dorian Karter <dkarter@users.noreply.github.com>
2025-08-23 23:30:23 -0500

    fix(nvim): prevent deno lsp from loading incorrectly

--- config/nvim/after/lsp/denols.lua
+++ config/nvim/after/lsp/denols.lua
@@ -1,5 +1,8 @@
+local lspconfig = require 'lspconfig'
+
 ---@type vim.lsp.Config
 return {
+  root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
   settings = {
     deno = {
       enable = true,
