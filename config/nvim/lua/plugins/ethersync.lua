7693e4db4d81d79ba7c92488d247a4a51c9d4e2f
Dorian Karter <dkarter@users.noreply.github.com>
2025-10-31 22:51:58 -0500

    fix(nvim)!: remove ethersync

--- config/nvim/lua/plugins/ethersync.lua
+++
@@ -1,6 +0,0 @@
--- Pair Programming with Ethersync https://github.com/ethersync/ethersync
----@type LazySpec
-return {
-  'ethersync/ethersync-nvim',
-  keys = { { '<leader>es', '<cmd>EthersyncJumpToCursor<cr>' } },
-}
