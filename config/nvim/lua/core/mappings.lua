0409d10dfe41702c9d8d190b2460c51e9058cd04
Dorian Karter <dkarter@users.noreply.github.com>
2025-11-02 21:14:16 -0600

    feat(nvim): add mappings for new Snacks gh (github) feature
    See https://github.com/folke/snacks.nvim/blob/main/docs/gh.md

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -736,6 +736,12 @@ M.snack_mappings = {
   -- open github page for line under cursor / selection
   { '<leader>go', function() Snacks.gitbrowse() end, mode = {'n', 'v'}, desc = '[G]ithub [o]pen Line' },
 
+  -- github
+  { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+  { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+  { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+  { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+
   -- lazygit
   { "<leader>gl", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
   { "<leader>gL", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
