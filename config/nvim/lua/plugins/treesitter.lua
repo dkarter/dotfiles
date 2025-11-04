fad0f25cc146a977da3223c11944e7a666d02bcd
Dorian Karter <dkarter@users.noreply.github.com>
2025-11-03 21:55:16 -0600

    fix(nvim): only load mise injections in mise files

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -102,6 +102,13 @@ return {
         enable = true,
       },
     },
+    init = function()
+      require('vim.treesitter.query').add_predicate('is-mise?', function(_, _, bufnr, _)
+        local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+        local filename = vim.fn.fnamemodify(filepath, ':t')
+        return string.match(filename, '.*mise.*%.toml$') ~= nil
+      end, { force = true, all = false })
+    end,
     config = function(_, opts)
       require('nvim-treesitter.configs').setup(opts)
 
