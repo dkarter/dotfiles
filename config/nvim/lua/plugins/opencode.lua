9eef7e76c5318d61dea0a4a9815d94aa73d8a762
Dorian Karter <dkarter@users.noreply.github.com>
2026-03-08 21:27:11 -0500

    fix(nvim): add opencode-tmux.nvim and update opencode.nvim
    This resolves the issue with latest opencode.nvim which dropped the
    providers including the one I was using - tmux provider.

--- config/nvim/lua/plugins/opencode.lua
+++ config/nvim/lua/plugins/opencode.lua
@@ -2,17 +2,26 @@
 ---@type LazySpec
 return {
   'NickvanDyke/opencode.nvim',
-  version = 'v0.4.0',
   ---@module 'opencode'
   ---@type opencode.Opts
-  opts = {
-    provider = {
-      enabled = 'tmux',
-    },
-  },
+  opts = {},
   config = function(_self, opts)
     -- idk why they do it this way - very weird
     vim.g.opencode_opts = opts
   end,
+
+  dependencies = {
+    -- tmux discovery and integration for opencode.nvim
+    {
+      'e-cal/opencode-tmux.nvim',
+      opts = {
+        options = '-h',
+        focus = false,
+        auto_close = false,
+        allow_passthrough = false,
+        find_sibling = true,
+      },
+    },
+  },
   keys = require('core.mappings').opencode_mappings,
 }
