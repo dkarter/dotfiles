05c4c7e10837bea71dc6ebe0d76689bba3dc7706
Dorian Karter <dkarter@users.noreply.github.com>
2026-03-08 20:50:25 -0500

    fix(zsh): enable forward agent for remote ssh sessions

--- config/zsh/op.zsh
+++ config/zsh/op.zsh
@@ -1,5 +1,8 @@
-# Enable SSH Agent for all hosts
-export SSH_AUTH_SOCK=~/.1password/agent.sock
+# Enable 1Password SSH Agent, but only if there isn't already a forwarded agent
+# (i.e. we're in a remote SSH session with ForwardAgent)
+if [[ -z $SSH_AUTH_SOCK || ! -S $SSH_AUTH_SOCK ]]; then
+  export SSH_AUTH_SOCK=~/.1password/agent.sock
+fi
 
 # prevent an issue with TELEPORT conflicting with 1password agent
 export TELEPORT_ADD_KEYS_TO_AGENT=no
