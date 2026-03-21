8e0606cdccdcbfc5bef2bad5cccafa0b532fc1a4
Dorian Karter <dkarter@users.noreply.github.com>
2026-03-20 19:36:50 -0500

    fix(nvim): make vim-test vimux strategy smarter
    Don't send the test command to a busy pane

--- config/nvim/lua/plugins/vim-test.lua
+++ config/nvim/lua/plugins/vim-test.lua
@@ -5,6 +5,19 @@ return {
   keys = require('core.mappings').vim_test_mappings,
   dependencies = { 'preservim/vimux' },
   init = function()
-    vim.g['test#strategy'] = 'vimux'
+    local smart_vimux = require 'core.smart_vimux'
+
+    pcall(vim.api.nvim_create_user_command, 'SmartVimuxSmoke', function(opts)
+      local cmd = opts.args ~= '' and opts.args or 'printf "smart-vimux smoke\n"'
+      smart_vimux.run(cmd)
+    end, { nargs = '*' })
+
+    local custom_strategies = vim.g['test#custom_strategies'] or {}
+    custom_strategies.smart_vimux = function(cmd)
+      smart_vimux.run(cmd)
+    end
+    vim.g['test#custom_strategies'] = custom_strategies
+
+    vim.g['test#strategy'] = 'smart_vimux'
   end,
 }
