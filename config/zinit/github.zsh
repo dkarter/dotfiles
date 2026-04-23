b80917d5c709a4c5d933c071638e1a08e82c3db2
Dorian Karter <dkarter@users.noreply.github.com>
2026-04-22 22:00:13 -0500

    fix(gh): opt out of telemetry

--- config/zinit/github.zsh
+++ config/zinit/github.zsh
@@ -1,3 +1,7 @@
+# https://cli.github.com/telemetry
+export DO_NOT_TRACK=true
+export GH_TELEMETRY=false
+
 # Set github token env vars from github cli - these are used by other CLIs
 if command -v gh &>/dev/null && gh auth status &>/dev/null; then
   export GITHUB_TOKEN=$(gh auth token)
