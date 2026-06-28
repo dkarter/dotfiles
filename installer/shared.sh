b5be8989d8ab377a52a09f5b872a53becbac3a8e
Dorian Karter <dkarter@users.noreply.github.com>
2026-06-28 13:01:57 -0500

    fix(setup): improve check for terminfo install

--- installer/shared.sh
+++ installer/shared.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO: move this to a task, including the script it runs
-if [[ ! -f "/tmp/terminfo/terminfo.src" ]]; then
+if ! infocmp tmux-256color >/dev/null 2>&1; then
   echo 'Installing TMux term info...'
   ./scripts/term.sh
 fi
--- scripts/term.sh
+++ scripts/term.sh
@@ -3,8 +3,17 @@
 set -euxo pipefail
 
 # download an updated terminfo file
-mkdir /tmp/terminfo
-pushd /tmp/terminfo
-curl -LO https://invisible-island.net/datafiles/current/terminfo.src.gz && gunzip terminfo.src.gz
-tic -xe alacritty-direct,tmux-256color terminfo.src
+tmp_dir="$(mktemp -d)"
+trap 'rm -rf "$tmp_dir"' EXIT
+
+mkdir -p ~/.terminfo/61 ~/.terminfo/74
+pushd "$tmp_dir"
+curl -fsSLO https://invisible-island.net/datafiles/current/terminfo.src.gz
+gunzip terminfo.src.gz
+
+if ! tic -xe alacritty-direct,tmux-256color terminfo.src; then
+  echo 'Could not compile alacritty-direct; installing tmux-256color only.' >&2
+  tic -xe tmux-256color terminfo.src
+fi
+
 popd
